05a0efb20233e7a355d94f0b8d65cdc8a3aa7db0
Alexander <alexander.r.ambrose@gmail.com>
2013-04-18 23:06:06 -0400

    Moar changes.

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use(require('connect-assets')());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
